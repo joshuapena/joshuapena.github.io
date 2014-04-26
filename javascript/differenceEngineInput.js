61064f341c127444cee1e2be2c9827444006a263
joshuapena <penacalata@gmail.com>
2014-04-26 02:34:47 -0700

    Update differenceEngineInput.js

--- javascript/differenceEngineInput.js
+++ javascript/differenceEngineInput.js
@@ -98,3 +98,12 @@ $('#clear').click(function() {
 $(document).ready(function() {
   initalUpdateNumbers();
 });
+
+// Allows only Number Keys
+function isNumberKey(evt) {
+  var charCode = (evt.which) ? evt.which : event.keyCode;
+  if (charCode > 31 && (charCode < 48 || charCode > 57)) {
+    return false;
+  }
+  return true;
+}

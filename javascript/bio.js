fc70fb9e40c9c25c4f7c45f416aca61656bd8297
joshuapena <penacalata@gmail.com>
2014-05-05 14:34:47 -0700

    changed bio page

--- javascript/bio.js
+++ javascript/bio.js
@@ -1,8 +1,38 @@
+var realPhoto;
+realPhoto = false;
+var photoClicked = 0;
+
 function enterPage() {
 	var name;
 	name = $("#name").val();
-	$("#welcome").html("Welcome " + name);
 	$("#nameText").fadeOut();
 	$("#name").fadeOut();
 	$("#enter").fadeOut();
+	$("#welcome").html("Welcome " + name);
+	$("#info").css('opacity', '1');
+	$("body").css('background-color', '#EEE');
+	$("#welcome").css("font-size", "16pt");
+	$("#welcome").css("font-family", "Ubuntu Mono");
 }
+
+$("#mePhoto").click(function() {
+	if (!realPhoto) {
+		$("#mePhoto").attr("src", "image/frame.jpg");
+		if (photoClicked < 1) {
+			$("#myPhotoText").html("Another Picture of Me");
+			photoClicked++;
+		} else {
+			$("#myPhotoText").html("Frame");
+		}
+		realPhoto = true;
+	} else {
+		$("#mePhoto").attr("src", "image/stickFigure.jpg");
+		if (photoClicked < 2) {
+			$("#myPhotoText").html("And Another");
+			photoClicked++;
+		} else {
+			$("#myPhotoText").html("Stick Figure");
+		}
+		realPhoto = false;
+	}
+});

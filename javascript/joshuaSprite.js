04b526bb04a51481cc2c4325cb061c9268c5ea68
joshuapena <penacalata@gmail.com>
2014-05-08 03:50:03 -0700

    Update joshuaSprite.js

--- javascript/joshuaSprite.js
+++ javascript/joshuaSprite.js
@@ -9,7 +9,6 @@ Shift : 16
 */
 
 var walking;
-walking = false;
 
 $(document).keydown(function(e) {
 	//var element = $(".joshua");
@@ -17,12 +16,11 @@ $(document).keydown(function(e) {
 		//alert("hi");
 		$(".joshua").css("width", "80px");
 		$(".joshua").css("background-image", "url(../image/joshua-sprite-walk.png");
-		walking = true;
-	} else {
-		walking = false;
 	}
 });
 
-if (!walking) {
-	$(".joshua").css("width", "88px");
-}
+$(document).keyup(function(e) {
+	if(e.keyCode == 32) {
+		$(".joshua").css("width", "88px");
+	}
+});

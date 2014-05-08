ce4b244721f51bc22d5e6e830de9f62ec5b73c37
joshuapena <penacalata@gmail.com>
2014-05-08 03:47:51 -0700

    Update joshuaSprite.js

--- javascript/joshuaSprite.js
+++ javascript/joshuaSprite.js
@@ -8,11 +8,21 @@ SpaceBar : 32
 Shift : 16
 */
 
+var walking;
+walking = false;
+
 $(document).keydown(function(e) {
 	//var element = $(".joshua");
 	if(e.keyCode == 32) {
 		//alert("hi");
 		$(".joshua").css("width", "80px");
 		$(".joshua").css("background-image", "url(../image/joshua-sprite-walk.png");
+		walking = true;
+	} else {
+		walking = false;
 	}
 });
+
+if (!walking) {
+	$(".joshua").css("width", "88px");
+}

69a773006b13abefc118cf69ef9b043e2de782d9
joshuapena <penacalata@gmail.com>
2014-05-11 01:35:01 -0700

    Update canvas.js

--- javascript/canvas.js
+++ javascript/canvas.js
@@ -110,25 +110,21 @@ var render = function () {
   ctx.fillText("Monsters caught : " + monstersCaught, 32, 32);
 };
 
-// The main game loop
 var main = function () {
-	var now = Date.now();
-	var delta = now - then;
-
-	update(delta / 1000);
-	render();
-
-	then = now;
-
-	// Request to do this again ASAP
-	requestAnimationFrame(main);
+  var now = Date.now();
+  var delta = now - then;
+  
+  update(delta / 1000);
+  render();
+  
+  then = now;
+  
+  requestAnimationFrame(main);
 };
 
-// Cross-browser support for requestAnimationFrame
 var w = window;
 requestAnimationFrame = w.requestAnimationFrame || w.webkitRequestAnimationFrame || w.msRequestAnimationFrame || w.mozRequestAnimationFrame;
 
-// Let's play this game!
 var then = Date.now();
 reset();
 main();

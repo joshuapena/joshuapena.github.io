fb1379c90f84c213d13f8b4b50d734f74a688426
joshuapena <penacalata@gmail.com>
2014-05-11 00:56:24 -0700

    Update canvas.js

--- javascript/canvas.js
+++ javascript/canvas.js
@@ -9,11 +9,13 @@ canvas.height = height;
 
 // Background Image
 var bgReady = false;
+/*
 var bgImage = new Image();
 bgImage.onload = function () {
   bgReady = true;
 };
 bgImage.src = "http://placekitten.com/512/480";
+*/
 
 // Hero Image
 var heroReady = false;

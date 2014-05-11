959333ad3d63b431139ddb6269b8271da9eb5b36
joshuapena <penacalata@gmail.com>
2014-05-11 01:21:06 -0700

    Update canvas.js

--- javascript/canvas.js
+++ javascript/canvas.js
@@ -79,12 +79,7 @@ var update = function (modifier) {
   }
   
   // Collision
-  if (
-    hero.x <= (monster.x + 32)
-    && monster.x <= (hero.x + 32)
-    && hero.y <= (monster.y + 32)
-    && monster.y <= (hero.y + 32)
-  ) {
+  if (hero.x <= (monster.x + 32) && monster.x <= (hero.x + 32) && hero.y <= (monster.y + 32) && monster.y <= (hero.y + 32)) {
     ++monstersCaught;
     reset();
   }

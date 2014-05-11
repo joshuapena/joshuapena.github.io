501e59d4bf96bbced67ad2242a9d69847bfa6a90
joshuapena <penacalata@gmail.com>
2014-05-11 01:36:38 -0700

    Update canvas.js

--- javascript/canvas.js
+++ javascript/canvas.js
@@ -33,84 +33,77 @@ monsterImage.src = "http://placekitten.com/30/32";
 
 // Game objects
 var hero = {
-  speed: 256,
-  x: 0,
-  y: 0
+	speed: 256 // movement in pixels per second
 };
-
-var monster = {
-  x: 0,
-  y: 0
-};
+var monster = {};
 var monstersCaught = 0;
 
 // Handle keyboard controls
 var keysDown = {};
 
 addEventListener("keydown", function (e) {
-  keysDown[e.keyCode] = true;
+	keysDown[e.keyCode] = true;
 }, false);
 
 addEventListener("keyup", function (e) {
-  delete keysDown[e.keyCode];
+	delete keysDown[e.keyCode];
 }, false);
 
+// Reset the game when the player catches a monster
 var reset = function () {
-  hero.x = canvas.width / 2;
-  hero.y = canvas.height / 2;
-  
-  monster.x = 32 + (Math.random() * (canvas.width - 64));
-  monster.y = 32 + (Math.random() * (canvas.height - 64));
-};
+	hero.x = canvas.width / 2;
+	hero.y = canvas.height / 2;
 
+	// Throw the monster somewhere on the screen randomly
+	monster.x = 32 + (Math.random() * (canvas.width - 64));
+	monster.y = 32 + (Math.random() * (canvas.height - 64));
+};
 
 // Update game objects
 var update = function (modifier) {
-	if (38 in keysDown) { // Player holding up
-		hero.y -= hero.speed * modifier;
-	}
-	if (40 in keysDown) { // Player holding down
-		hero.y += hero.speed * modifier;
-	}
-	if (37 in keysDown) { // Player holding left
-		hero.x -= hero.speed * modifier;
-	}
-	if (39 in keysDown) { // Player holding right
-		hero.x += hero.speed * modifier;
-	}
-
-	// Are they touching?
-	if (
-		hero.x <= (monster.x + 32)
-		&& monster.x <= (hero.x + 32)
-		&& hero.y <= (monster.y + 32)
-		&& monster.y <= (hero.y + 32)
-	) {
-		++monstersCaught;
-		reset();
-	}
+  if (38 in keysDown) {
+    hero.y -= hero.speed * modifier;
+    document.write("up");
+  }
+  if (40 in keysDown) {
+    hero.y += hero.speed * modifier;
+    document.write("down");
+  }
+  if (37 in keysDown) {
+    hero.x -= hero.speed * modifier;
+    document.write("left");
+  }
+  if (39 in keysDown) {
+    hero.x += hero.speed * modifier;
+    document.write("right");
+  }
+  
+  // Collision
+  if (hero.x <= (monster.x + 32) && monster.x <= (hero.x + 32) && hero.y <= (monster.y + 32) && monster.y <= (hero.y + 32)) {
+    ++monstersCaught;
+    reset();
+  }
 };
 
-// Draw everything
+// Render
 var render = function () {
-	if (bgReady) {
-		ctx.drawImage(bgImage, 0, 0);
-	}
-
-	if (heroReady) {
-		ctx.drawImage(heroImage, hero.x, hero.y);
-	}
-
-	if (monsterReady) {
-		ctx.drawImage(monsterImage, monster.x, monster.y);
-	}
-
-	// Score
-	ctx.fillStyle = "rgb(250, 250, 250)";
-	ctx.font = "24px Helvetica";
-	ctx.textAlign = "left";
-	ctx.textBaseline = "top";
-	ctx.fillText("Goblins caught: " + monstersCaught, 32, 32);
+  if (bgReady) {
+    ctx.drawImage(bgImage, 0, 0);
+  }
+  
+  if (heroReady) {
+    ctx.drawImage(heroImage, hero.x, hero.y);
+  }
+  
+  if (monsterReady) {
+    ctx.drawImage(monsterImage, monster.x, monster.y);
+  }
+  
+  ctx.fillStyle = "rgb(250, 250, 250)";
+  ctx.font = "24px Helvetica";
+  ctx.textAlign = "left";
+  ctx.textBaseline = "top";
+  ctx.fillText("Monsters caught : " + monstersCaught, 32, 32);
 };
 
 var main = function () {

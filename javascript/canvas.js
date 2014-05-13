027a3589ac86b52db6bf87b112fb446edae6bb49
joshuapena <penacalata@gmail.com>
2014-05-13 11:07:14 -0700

    Update canvas.js

--- javascript/canvas.js
+++ javascript/canvas.js
@@ -129,7 +129,7 @@ var main = function () {
   countDown = nowSwitch - thenSwitch;
   
   update(delta / 1000);
-  if (countDown >= 1500) {
+  if (countDown >= 1100) {
     thenSwitch = nowSwitch;
     reset();
   }

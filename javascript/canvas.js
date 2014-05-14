55863ce75ab9d3a78d32c724cfbdfd147f026b0e
joshuapena <penacalata@gmail.com>
2014-05-13 21:04:29 -0700

    Update canvas.js

--- javascript/canvas.js
+++ javascript/canvas.js
@@ -137,7 +137,7 @@ var main = function () {
   
   countDown = nowSwitch - thenSwitch;
   
-  if (countDown >= 1000) {
+  if (countDown >= 10000) {
     lives--;
     if (lives ==  0) {
       gameOver();

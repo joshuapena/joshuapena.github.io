256123459e2d64b602a02bd9b67dbe6428cb6436
joshuapena <penacalata@gmail.com>
2014-05-13 21:06:56 -0700

    Update canvas.js

--- javascript/canvas.js
+++ javascript/canvas.js
@@ -120,7 +120,7 @@ var render = function () {
   ctx.textBaseline = "top";
   ctx.fillText("Kitties caught : " + kittyCaught, 32, 32);
   ctx.fillText("Lives : " + lives, 365, 32);
-  ctx.fillText("Timer : " + parseInt(countDown / 1000), 32, 64);
+  ctx.fillText("Timer : " + parseInt(countDown / 100), 32, 64);
 };
 
 var gameOver = function () {

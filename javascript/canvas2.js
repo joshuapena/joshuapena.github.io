dbb02d85be17e460acdbfd2ea6695b9f2d305e89
joshuapena <penacalata@gmail.com>
2014-05-13 20:03:05 -0700

    Update canvas2.js

--- javascript/canvas2.js
+++ javascript/canvas2.js
@@ -135,7 +135,7 @@ var main = function () {
   update(delta / 1000);
   if (countDown >= 20000) {
     lives--;
-    if (lives ==  0) {
+    if (lives ==  timer) {
       gameOver();
     }
     thenSwitch = nowSwitch;
@@ -155,6 +155,7 @@ requestAnimationFrame = w.requestAnimationFrame || w.webkitRequestAnimationFrame
 var then, thenSwitch, nowSwitch;
 var countDown = 0;
 var lives = 9;
+var timer = 1200;
 
 var start = function() {
   then = thenSwitch = nowSwitch = Date.now();

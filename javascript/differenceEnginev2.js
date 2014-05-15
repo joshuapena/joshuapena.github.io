f2f9828b106c5355ad7edf41f6c70cbd4ed53904
joshuapena <penacalata@gmail.com>
2014-05-15 16:47:31 -0700

    Update differenceEnginev2.js

--- javascript/differenceEnginev2.js
+++ javascript/differenceEnginev2.js
@@ -92,8 +92,8 @@ $('#clear').click(function() {
       parseInt(document.getElementById("num6" + i).value);
     //document.getElementById("num" + 6 + i).value = 5; //$("#num" + 6 + (i + 1)).value;
   }
-  for (i = 0; i < 6; i++) {
-    for (var j = 0; j < 7; j++) {
+  for (i = 0; i < 8; i++) {
+    for (var j = 0; j < 6; j++) {
       document.getElementById("num" + j + i).value = i;
       //document.getElementById("num" + j + (i + 1)).value = 
       //  parseInt(document.getElementById("num" + j + (i + 1)).value) + 

b7afc2b597252180d82f81cfa76989d93e02b23a
joshuapena <penacalata@gmail.com>
2014-05-21 01:16:21 -0700

    Update canvasAnimation.js

--- javascript/canvasAnimation.js
+++ javascript/canvasAnimation.js
@@ -85,7 +85,6 @@ var reset = function () {
 var update = function (modifier) {
   if (38 in keysDown) {
     hero.y -= hero.speed * modifier;
-    joshuaState = "walk";
   }
   if (40 in keysDown) {
     hero.y += hero.speed * modifier;
@@ -95,6 +94,12 @@ var update = function (modifier) {
   }
   if (39 in keysDown) {
     hero.x += hero.speed * modifier;
+  }
+  
+  if (37 || 38 || 39 || 40 in keysDown) {
+    joshuaState = "walk";
+  } else {
+    joshuaState = "idle";
   }
   
   // Stop from going off the page

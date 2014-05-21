e869df37b3bbccb6d431d66d509f3c404d2363ef
joshuapena <penacalata@gmail.com>
2014-05-21 00:55:20 -0700

    Update canvasAnimation.js

--- javascript/canvasAnimation.js
+++ javascript/canvasAnimation.js
@@ -85,19 +85,19 @@ var reset = function () {
 var update = function (modifier) {
   if (38 in keysDown) {
     hero.y -= hero.speed * modifier;
-    state = walk;
+    state = "walk";
   }
   if (40 in keysDown) {
     hero.y += hero.speed * modifier;
-    state = walk;
+    state = "walk";
   }
   if (37 in keysDown) {
     hero.x -= hero.speed * modifier;
-    state = walk;
+    state = "walk";
   }
   if (39 in keysDown) {
     hero.x += hero.speed * modifier;
-    state = walk;
+    state = "walk";
   }
   
   // Stop from going off the page

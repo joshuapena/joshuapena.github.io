8d32677f7ce410c670a212f2248ddd64e69ffc48
joshuapena <penacalata@gmail.com>
2014-06-02 11:01:45 -0700

    Update index.js

--- attackOfTheSpaceCat/js/index.js
+++ attackOfTheSpaceCat/js/index.js
@@ -23,7 +23,7 @@ var heroImage = new Image();
 heroImage.onload = function () {
 	heroReady = true;
 };
-heroImage.src = "../assets/images/spaceCat.png";
+heroImage.src = "assets/images/spaceCat.png";
 
 // Kitty Image
 var kittyReady = false;
@@ -31,7 +31,7 @@ var kittyImage = new Image();
 kittyImage.onload = function () {
 	kittyReady = true;
 };
-kittyImage.src = "../assets/images/catDeathStar.png";
+kittyImage.src = "assets/images/catDeathStar.png";
 
 // Game objects
 var hero = {

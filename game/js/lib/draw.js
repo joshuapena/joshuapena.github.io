ebead922d6d057315c657cfd1ebf5cdfa32a5a28
joshuapena <penacalata@gmail.com>
2014-06-16 01:17:51 -0700

    Update draw.js

--- game/js/lib/draw.js
+++ game/js/lib/draw.js
@@ -18,4 +18,14 @@ var draw = function(world) {
 			});
 		}
 	);
+	
+	if (world.end) {
+		world.ctx.globalAlpha = 0.4;
+		world.ctx.fillStyle = "#333";
+		world.ctx.fillRect(0, 0, 600, 300);
+		world.ctx.fillStyle = "#FFF";
+		world.ctx.font = "50px Ubuntu Mono";
+		world.ctx.globalAlpha = 1.0;
+		world.ctx.fillText("You Win", 200, 150);
+	}
 }
